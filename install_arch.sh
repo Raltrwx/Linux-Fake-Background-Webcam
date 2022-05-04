260c9e557346cc5cd1b8e4c394b12caeec9e9aba
Matt/Raltrwx <Raltrwx@gmail.com>
2022-05-04 17:22:05 -0600

    added user-level systemd service, made install_arch.sh the only script you need to run on arch

--- install_arch.sh
+++ install_arch.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+Don't run this as root
 trizen -S linux-headers v4l2loopback-dkms python python-pip miniconda3
+sudo ./v4l2loopback-install.sh
 conda env create -n webcam --file environment.yaml
+mkdir -p ~/.config/systemd/user
+cp fakewebcam.service ~/.config/systemd/user/
+sudo systemctl daemon-reload
+systemctl --user enable fakewebcam.service
+echo "Installed 
+echo "User-Level systemd service installed & enabled"
+echo "To start it: systemctl --user start fakewebcam.service"
+echo "It pauses with no consumers, but feel free to shut it off with: systemctl --user stop fakewebcam.service"
