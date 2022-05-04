c1fefbcd25afbfa5d0890c547ddd3a4a1619c0ce
Matt/Raltrwx <Raltrwx@gmail.com>
2022-05-04 17:42:21 -0600

    finished install

--- install_arch.sh
+++ install_arch.sh
@@ -7,7 +7,7 @@ mkdir -p ~/.config/systemd/user
 sudo cp fakewebcam.service ~/.config/systemd/user/
 sudo systemctl daemon-reload
 systemctl --user enable fakewebcam.service
-echo "Installed 
+echo "Installed Deps+v4l2loopback"
 echo "User-Level systemd service installed & enabled"
 echo "To start it: systemctl --user start fakewebcam.service"
 echo "It pauses with no consumers, but feel free to shut it off with: systemctl --user stop fakewebcam.service"
